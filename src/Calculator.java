3c7ae68ecacfc545fe9e813f67f5758036b5cc5c
NatalyaTsoy <46766234+NatalyaTsoy@users.noreply.github.com>
2019-01-19 10:03:38 -0800

    Add multiply method.

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,5 +4,12 @@ public class Calculator {
         return num1+num2;
     }
 
-    public static double subtruct(int num1, int num2){double sum = num1+ num2; return sum;}
+    public static double subtruct(int num1, int num2){
+        double sum = num1+ num2; 
+        return sum;
+    }
+    
+    public static double multiply(int num1. int num2){
+        return num1*num2;
+    }
 }
