f6a80bab7f525b566960f96011995cca44b76d2c
NatalyaTsoy <46766234+NatalyaTsoy@users.noreply.github.com>
2019-01-19 13:37:55 -0800

    add division method

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,4 +12,8 @@ public class Calculator {
     public static double multiply(int num1, int num2){
         return num1*num2;
     }
+    
+    public static double delete(int num1, int num2){
+        return num1/num2;
+    }
 }
