aa6e1d064468be36bf5179176abfe216305f7ef8
Natalya Tsoy <natashka3070@gmail.com>
2019-01-19 09:49:46 -0800

    subtruct method2

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,5 +4,5 @@ public class Calculator {
         return num1+num2;
     }
 
-    public static double subtruct(int num1, int num2){return num1-num2;}
+    public static double subtruct(int num1, int num2){double sum = num1+ num2; return sum;}
 }
